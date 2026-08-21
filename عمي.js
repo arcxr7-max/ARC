d538cbdd2850eb6760bdb7f7cb884461e8f673aa
arcxr7-max <arcxr7@gmail.com>
2026-08-21 20:47:38 +0100

    Update عمي.js

--- عمي.js
+++ عمي.js
@@ -1,5 +1,5 @@
 {
-  "image": "https://i.imgur.com/8dE8w1H.jpeg",
+  "image": "blob:https://www.image2url.com/87cbe83b-0292-45c8-bc7f-98e8b3c22e52",
   "caption": "ده سونغ مش سون لاتنسى انه عمك 🙂",
   "reaction": "💀"
 }
